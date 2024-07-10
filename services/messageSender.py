8276f13927c13d8900225dc130aeab348f5c3f51
EmoMeza <emeza2019@inf.udec.cl>
2024-07-10 19:35:08 -0400

    ♻️ Update Docker service ports to use 5040 instead of 5000

--- services/messageSender.py
+++ services/messageSender.py
@@ -4,7 +4,7 @@ import services.message_pb2_grpc as message_pb2_grpc
 import requests
 
 def sendREST(mensaje):
-        response = requests.post('http://localhost:5000/message', json={"texto": mensaje})
+        response = requests.post('http://localhost:5040/message', json={"texto": mensaje})
         print("Server response:", response.text)
         response = requests.get('http://localhost:6000/output')
         print("Websocket response:", response.text)
