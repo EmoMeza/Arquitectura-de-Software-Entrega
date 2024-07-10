66574805a564c53c1bb3cafcdcf6fa95d7e587c0
EmoMeza <emeza2019@inf.udec.cl>
2024-07-10 00:31:07 -0400

    Update database connection to use MongoDB instead of PostgreSQL in GRPC

--- grpc-server/server.py
+++ grpc-server/server.py
@@ -1,19 +1,17 @@
 # servidor_grpc.py
 from concurrent import futures
 import grpc
-import psycopg2
+import pymongo
 from protos import my_service_pb2_grpc, my_service_pb2
 
 class MyServiceServicer(my_service_pb2_grpc.MyServiceServicer):
     def MyMethod(self, request, context):
         message = request.message
         # Almacenar en la base de datos
-        conn = psycopg2.connect(dbname="mi_base_de_datos", user="usuario", password="contraseña", host="db")
-        cur = conn.cursor()
-        cur.execute("INSERT INTO messages (message) VALUES (%s)", (message,))
-        conn.commit()
-        cur.close()
-        conn.close()
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        db = client["mi_base_de_datos"]
+        collection = db["messages"]
+        collection.insert_one({"message": message})
         # Notificar al cliente WebSocket
         # Implementar la lógica de notificación
         return my_service_pb2.MyResponse(status="Mensaje recibido")
